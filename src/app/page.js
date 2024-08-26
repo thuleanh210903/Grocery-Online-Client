a4cc38f0f4acdcb1ccb1dd1ee6f77ccd0040f689
ThuLe <thuanhle219@gmail.com>
2024-08-26 13:51:47 +0700

    feat: Implement all product list for home page

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,16 @@ import Image from "next/image";
 import Header from "./_components/Header";
 import GlobalApi from "./_utils/GlobalApi";
 import CategoryList from "./_components/CategoryList";
+import ProductList from "./_components/ProductList";
 
 export default async function Home() {
   const categoryList = await GlobalApi.getCategoryList()
+  const productList = await GlobalApi.getProductList()
   return (
-    <div>
+    <div className="pl-20">
       <Header />
       <CategoryList categoryList={categoryList}/>
+      <ProductList productList = {productList} />
     </div>
   );
 }
